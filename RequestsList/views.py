204fa0e33c65ad29b1a9a532d35b3fbed56a527d
Ilya Zakharov <masterrus417@gmail.com>
2024-03-01 17:55:40 +0500

    initial

--- RequestsList/views.py
+++ RequestsList/views.py
@@ -10,15 +10,18 @@ def requests_list(request):
     requests_list_labels = RequestsList.objects.values_list('label_list').first()
     
     requsets_values = {}
-    labels_list = requests_list_labels[0].split(';')
+    labels_list = []
     
-    requests_list_values = RequestsList.objects.values_list('entity_id','attr_list').all()
+    if requests_list_labels != None:
+        labels_list = requests_list_labels[0].split(';')
+        requests_list_values = RequestsList.objects.values_list('entity_id','attr_list').all()
 
     
-    for val in requests_list_values:
-        requsets_values[val[0]] = val[1].split(';')
+        for val in requests_list_values:
+            requsets_values[val[0]] = val[1].split(';')
     
     print(requsets_values)
 
     return render(request,'requests_list.html',{'labels_list' : labels_list, 'requsets_values' : requsets_values})
+
 
